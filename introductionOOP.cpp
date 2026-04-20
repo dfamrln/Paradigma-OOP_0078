c43895bcef6d2b75431d8c5bc158c7722c2bd1fe
dfamrln <mhdffa1@gmail.com>
2026-04-20 10:18:55 +0700

    menambahkan class dan void

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa{
+    public  :
+        int nim;
+        string nama;
+        float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim endl;
+    }
+};
