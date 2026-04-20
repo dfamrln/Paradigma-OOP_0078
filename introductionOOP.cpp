2f89065b6da1c72bc0073821b9869a4cfc6cc319
dfamrln <mhdffa1@gmail.com>
2026-04-20 10:19:56 +0700

    melengkapai isi void

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,6 +8,8 @@ class Mahasiswa{
         float nilai;
 
     void printData(){
-        cout << "NIM : " << nim endl;
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
     }
 };
